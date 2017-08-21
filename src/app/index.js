fc395550509168e680d7aa7c5334436dfb0ed6f7
Yewhral <zabrzyjewski@gmail.com>
2017-08-21 12:33:06 +0200

    React state

--- src/app/index.js
+++ src/app/index.js
@@ -5,15 +5,30 @@ const ReactDOM = require('react-dom');
 // React way
 
 let TodoComponent = React.createClass({
+
+    getInitialState: function(){
+        return {
+        todos: ['do thing A', 'do thing B', 'do thing C']
+        }
+    },
+
     render: function(){
         return (
-            <div>
+            <div id="todo-list">
+                <p>Test text</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
+
             <h1>Hello!</h1>
             <p>{this.props.message}</p>
                 <p>Fish: {this.props.fish.name}</p>
                 <p>Smell: {this.props.fish.smellFactor}</p>
                 <p>Price: {this.props.fish.price} $</p>
             </div>
+
         );
     }
 });
