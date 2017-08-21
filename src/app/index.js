899ce307dd5c6430706e7c69992892da7342789c
Yewhral <zabrzyjewski@gmail.com>
2017-08-21 14:24:28 +0200

    React code modularized, new component added

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const TodoItem = require('./todoItem');
+require('./css/index.css');
+
 // Create component
 // React way
 
@@ -56,22 +59,7 @@ let TodoComponent = React.createClass({
     }
 });
 
-//Second component
-let TodoItem = React.createClass({
-   render: function(){
-       return(
-           <li>
-               <div className="todo-item">
-               <span className="item-name">{this.props.item}</span>
-               <span className="item-delete" onClick={this.handleDelete}> X </span>
-               </div>
-           </li>
-       )
-   },
-   handleDelete: function(){
-       this.props.onDelete(this.props.item);
-   }
-});
+
 
 let myFish ={ name: 'Salmon', smellFactor: 'Smelly smell', price:'19.90'};
 
---
+++ src/app/todoItem.js
@@ -0,0 +1,21 @@
+const React = require('react');
+require('./css/todoItem.css');
+
+//Second component
+let TodoItem = React.createClass({
+    render: function(){
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}> X </span>
+                </div>
+            </li>
+        )
+    },
+    handleDelete: function(){
+        this.props.onDelete(this.props.item);
+    }
+});
+
+module.exports = TodoItem;
